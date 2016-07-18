3e63efcdbb7c477e8e19a5e72e31a101e3fbaaac
LAVANYA2795 <mlavanya2795@gmail.com>
2016-07-18 18:51:17 +0530

    Create reverse.c

---
+++ reverse.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n,rev=0;
+clrscr();
+printf("Enter  a number to reverse\n");
+scanf("%d",&n);
+while(n!=0)
+{
+rev=rev*10;
+rev=rev+n%10;
+n=n/10;
+}
+printf("Reverse of entered number is=%d\n",rev);
+getch();
+return 0;
+}
